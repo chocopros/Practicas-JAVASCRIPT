cb142d55cc4f1ab78fbf68d9d0194092155778a7
unknown <jesuschock93@gmail.com>
2022-11-02 14:23:59 -0400

    miercoles

---
+++ mundo pc/mundoPc.ejercicio.js
@@ -0,0 +1,25 @@
+//? APLICATION "MUNDO PC"
+
+class DeviceIn {
+
+    constructor(typeIN, brand){
+        this._typeIN = typeIN;
+        this._brand = brand;
+    }
+
+    get typeIN(){
+        return this._typeIN;
+    }
+    set typeIN(newTypeIN){
+        this._typeIN;
+    }
+
+    get brand(){
+        return this._brand;
+    }
+    set brand(newBrand){
+        this._brand;
+    }
+
+
+}
